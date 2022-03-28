5c9ac6cca921fa8286441f9dcd7cdab93479da9c
longyadong <202780181@qq.com>
2022-03-28 09:52:28 +0800

    fix: 更新http请求响应拦截设置

--- src/middlewares/httpInterceptors.ts
+++ src/middlewares/httpInterceptors.ts
@@ -1,21 +1,21 @@
 import { message } from 'antd';
 import cookie from 'react-cookies';
 
-//拦截器-请求前拦截
+// 拦截器-请求前拦截
 const RequestInterceptor = (url: string, options: any) => {
   const o = options;
-  //如果不是登陆页 headers里添加 username
-  //headers 里面的内容是和后端协商好的
+  // 如果不是登陆页 headers里添加 username
+  // headers 里面的内容是和后端协商好的
   o.headers = {
     ...options.headers,
-    token: cookie.load('token') || undefined,
+    token: cookie?.load('token'),
   };
   return {
     options: o,
   };
 };
 
-//拦截器-响应后拦截
+// 拦截器-响应后拦截
 const ResponseInterceptors = async (response: Response): Promise<any> => {
   const res = await response.clone().json();
   if (res?.result) {
